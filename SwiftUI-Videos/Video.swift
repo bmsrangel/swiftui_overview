5a9789dd13a3518daf82f52db9f9a36326235d5e
bmsrangel <bmsrangel@hotmail.com>
2023-05-23 14:16:12 -0300

    Upload project codebase to GitHub

---
+++ SwiftUI-Videos/Video.swift
@@ -0,0 +1,31 @@
+//
+//  Video.swift
+//  SwiftUI-Videos
+//
+//  Created by Bruno Rangel on 23/05/23.
+//
+
+import Foundation
+
+struct Video: Identifiable {
+    var id = UUID()
+    var imageName: String
+    var title: String
+    var uploadDate: String
+}
+
+enum VideoList {
+    static let topTwelve = [
+        Video(imageName: "37-tips", title: "37 Tips for Jr. Software Developers", uploadDate: "October 4, 2019"),
+        Video(imageName: "90-90", title: "The 90/90 Rile", uploadDate: "January 24, 2019"),
+        Video(imageName: "2018-setup", title: "Aluna - Monitor Your Asthma", uploadDate: "May 12, 2019"),
+        Video(imageName: "average-dev", title: "I'm an AVERAGE Software Developer", uploadDate: "January 28, 2020"),
+        Video(imageName: "child-vc", title: "How to use Child View Controllers", uploadDate: "October 1, 2019"),
+        Video(imageName: "hig", title: "Apple's Humen Interface Guidelines", uploadDate: "December 5, 2019"),
+        Video(imageName: "hired", title: "Hired's 2020 State of Software Engineering", uploadDate: "February 13, 2020"),
+        Video(imageName: "lazy", title: "How to Use Lazy in Swift", uploadDate: "February 27, 2020"),
+        Video(imageName: "macaw", title: "How to Create a Bar Chart in Swift", uploadDate: "May 7, 2019"),
+        Video(imageName: "nav-controller", title: "iOS Navigation Controller Tutorial", uploadDate: "March 28, 2020"),
+        Video(imageName: "portfolio", title: "15 iOS Developer Portfolios", uploadDate: "February 15, 2020")
+    ]
+}
