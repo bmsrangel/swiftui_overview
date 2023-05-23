15dfa6462f83917bfc4aa53a60f323e4400bdfed
bmsrangel <bmsrangel@hotmail.com>
2023-05-23 11:35:39 -0300

    Initial Commit

---
+++ SwiftUI-Videos/SwiftUI_VideosApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_VideosApp.swift
+//  SwiftUI-Videos
+//
+//  Created by Bruno Rangel on 23/05/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_VideosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
